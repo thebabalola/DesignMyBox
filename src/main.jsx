40999ff8723e6c3e340655bf47a8533ac4da2d34
thebabalola <t.babalolajoseph@gmail.com>
2025-06-14 22:48:29 +0100

    resolving index.css import issue in deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './styles/index.css';
-import './styles/custom.css';
+import '../styles/Index.css';
+import '../styles/custom.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
